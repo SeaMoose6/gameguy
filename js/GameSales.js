9ac7851ede357b4bc0daa8d4cca3ffc7012514b2
SeaMoose6 <tophtoph05@gmail.com>
2024-05-09 12:38:29 -0400

    js fix

--- js/GameSales.js
+++ js/GameSales.js
@@ -1,3 +1,17 @@
+const header = document.querySelector('.header')
+const hamburger = document.querySelector('.hamburger')
+const menu = document.querySelector('.menu')
+
+hamburger.addEventListener('click', ()=>{
+    console.log("clicked")
+    if (menu.style.display == "none"){
+        menu.style.display = "block"
+    }else{
+        menu.style.display = "none"
+    }
+})
+
+
 fetch("https://steamdb.info/sales/?min_reviews=500&min_rating=60&min_discount=50", {
     mode: 'no-cors'
 })
