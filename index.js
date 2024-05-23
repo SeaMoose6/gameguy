26ee909938e2e92ddf2aa445b577457f1be57be1
SeaMoose6 <tophtoph05@gmail.com>
2024-05-22 23:30:34 -0400

    at home

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const header = document.querySelector('.header')
 const hamburger = document.querySelector('.hamburger')
 const menu = document.querySelector('.menu')
+const learn = document.querySelector(".learn")
+const CTA = document.querySelector(".CTA")
 
-// hamburger.addEventListener('click', ()=>{
-//     console.log("clicked")
-//     if (menu.style.display == "none"){
-//         menu.style.display = "block"
-//     }else{
-//         menu.style.display = "none"
-//     }
-// })
+hamburger.addEventListener('click', ()=>{
+     console.log("clicked")
+     if (menu.style.display == "none"){
+         menu.style.display = "block"
+     }else{
+         menu.style.display = "none"
+     }
+ })
+
+learn.addEventListener('click', ()=>{
+    console.log("clicked")
+    if (CTA.style.display == "none"){
+        CTA.style.display = "block"
+    }else{
+        CTA.style.display = "none"
+    }
+})
